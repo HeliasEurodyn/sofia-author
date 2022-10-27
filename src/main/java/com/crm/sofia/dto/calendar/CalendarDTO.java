e686e9874cd3a51690fb6c2c3f64f86387e420d7
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2022-10-27 18:04:55 +0300

    CalendarDTO Created

---
+++ src/main/java/com/crm/sofia/dto/calendar/CalendarDTO.java
@@ -0,0 +1,29 @@
+package com.crm.sofia.dto.calendar;
+
+import com.crm.sofia.dto.common.BaseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
+public class CalendarDTO extends BaseDTO {
+
+    @NotNull
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    private String icon;
+}
